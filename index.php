6cc633812b9f659073a753ce300a88782ad486e2
jatkore <kazachienko01@gmail.com>
2025-04-22 12:45:22 +0500

    check

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Магазин fЧасов</title>
+    <title>Магазин Часов</title>
     <style>
         /* Общие стили */
         body {
